a5580dbb95bb25545b46b1295cd023e695db7562
Jaydeep Rajpurohit <jaydeeppurohit1996@gmail.com>
2023-01-06 20:39:13 +0530

    WIP

--- exchanges/apex/apex.go
+++ exchanges/apex/apex.go
@@ -1,7 +1,12 @@
 package apex
 
 import (
+	"context"
+	"net/http"
+	"time"
+
 	exchange "github.com/thrasher-corp/gocryptotrader/exchanges"
+	"github.com/thrasher-corp/gocryptotrader/exchanges/request"
 )
 
 // Apex is the overarching type across this package
@@ -10,12 +15,53 @@ type Apex struct {
 }
 
 const (
-	apexAPIURL     = ""
-	apexAPIVersion = ""
+	apexAPIURL     = "https://pro.apex.exchange/api/"
+	apexAPIVersion = "/v1"
 
 	// Public endpoints
+	apexSystemTime         = "/time"
+	apexAllConfigData      = "/symbols"
+	apexMarketDepth        = "/depth"
+	apexLatestTrades       = "/trades"
+	apexKlines             = "/klines"
+	apexTicker             = "/ticker"
+	apexFundingRateHistory = "/history-funding"
+	apexCheckVersion       = "/check-version"
+	apexCheckUserExists    = "/check-user-exist"
+	apexAdvertisementData  = "/ads-banner"
 
 	// Authenticated endpoints
 )
 
-// Start implementing public and private exchange API funcs below
+// GetSystemTime gets system time
+func (a *Apex) GetSystemTime(ctx context.Context) (time.Time, error) {
+	resp := struct {
+		Data struct {
+			Time int64 `json:"time"`
+		} `json:"result"`
+		Error
+	}{}
+	return time.Unix(resp.Data.Time, 0), a.SendHTTPRequest(ctx, exchange.RestSpot, apexSystemTime, publicSpotRate, &resp)
+}
+
+// SendHTTPRequest sends an unauthenticated request
+func (a *Apex) SendHTTPRequest(ctx context.Context, ePath exchange.URL, path string, f request.EndpointLimit, result UnmarshalTo) error {
+	endpointPath, err := a.API.Endpoints.GetURL(ePath)
+	if err != nil {
+		return err
+	}
+
+	err = a.SendPayload(ctx, f, func() (*request.Item, error) {
+		return &request.Item{
+			Method:        http.MethodGet,
+			Path:          apexAPIVersion + endpointPath + path,
+			Result:        result,
+			Verbose:       a.Verbose,
+			HTTPDebugging: a.HTTPDebugging,
+			HTTPRecording: a.HTTPRecording}, nil
+	})
+	if err != nil {
+		return err
+	}
+	return result.GetError()
+}
--- exchanges/apex/apex_types.go
+++ exchanges/apex/apex_types.go
@@ -1,1 +1,22 @@
 package apex
+
+import "errors"
+
+// UnmarshalTo acts as interface to exchange API response
+type UnmarshalTo interface {
+	GetError() error
+}
+
+// Error defines all error information for each request
+type Error struct {
+	ReturnCode int64  `json:"code"`
+	ReturnMsg  string `json:"msg"`
+}
+
+// GetError checks and returns an error if it is supplied.
+func (e Error) GetError() error {
+	if e.ReturnCode != 0 && e.ReturnMsg != "" {
+		return errors.New(e.ReturnMsg)
+	}
+	return nil
+}
